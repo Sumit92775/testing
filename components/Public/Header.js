acb5072aced425881297919badf6653918b5288a
Sumit <dy4366190@gmail.com>
2021-09-21 22:40:37 +0530

    changes

--- components/Public/Header.js
+++ components/Public/Header.js
@@ -1,9 +1,11 @@
 import useTranslation from 'next-translate/useTranslation';
 import { UserOutlined } from '@ant-design/icons';
-import { Avatar, Button } from 'antd';
+import { Avatar, Button, Menu } from 'antd';
 import styles from './Header.module.scss';
 import Image from 'next/image';
 import Link from 'next/link'
+
+const { SubMenu } = Menu;
 
 const Header = () => {
     const { t } = useTranslation('common');
@@ -24,12 +26,43 @@ const Header = () => {
                 <span>Arabic</span>
             </div>
             <div className="header-actions">
-                <Link href="/signup" passHref={true}>
+                {/* <Link href="/signup" passHref={true}>
                     <Button className="ant-btn default medium mr-14">{ t('Sign In') }</Button>
                 </Link>
                 <Link href="/popup" passHref={true}>
                     <Button className="ant-btn primary medium">{ t('Sign Up') }</Button>
-                </Link>
+                </Link> */}
+                <div>
+                    <Menu mode="horizontal" className="user-actions transparent-bg">
+                        <SubMenu key="SubMenu" icon={<Avatar className="mr-5" size={22} icon={<UserOutlined />} />} title="Halais">
+                            <Menu.Item key="setting-1">
+                                <Link href={ process.env.base_url + "login" } passHref={true}>
+                                    My Bookings
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="giftcard-1">
+                                <Link href={ process.env.base_url + "reviews" } passHref={true}>
+                                    My Reviews
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="setting-1">
+                                <Link href={"payments/wallet" } passHref={true}>
+                                    My Wallets
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="setting-1">
+                                <Link href={ process.env.base_url + "gift-cards" } passHref={true}>
+                                    Gift Cards
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="giftcard-1">
+                                <Link href={ process.env.base_url + "" } passHref={true}>
+                                    Logout
+                                </Link>
+                            </Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </div>
 
             </div>
         </div>
